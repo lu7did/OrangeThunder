ebdb3e2a15c1913b29c47e50b607f40aef681060
lu7did <lu7did@gmail.com>
2019-02-12 11:44:15 -0300

    maint

--- bash/host.maint.sh
+++ bash/host.maint.sh
@@ -9,6 +9,35 @@
 # Short-Description: Start the acrux daily maintenance
 ### END INIT INFO
 
+#*--------------------------------------------------------------------------------------------
+#* Copy a file, place over the previous if it exists
+#*--------------------------------------------------------------------------------------------
+copyFile() {
+echo `date`"copyFile: [$(hostname)] $1" 2>&1 >>$BPATH$LOG
+cd /home/pi/Dropbox-Uploader
+./dropbox_uploader.sh mkdir /SouthernCroix/$(hostname)
+./dropbox_uploader.sh upload $1 /SouthernCroix/$(hostname)/$1
+
+}
+#*--------------------------------------------------------------------------------------------
+#* Store a file, create a daily version to preserver previous
+#*--------------------------------------------------------------------------------------------
+storeFile() {
+echo `date`" storeFile: [$(hostname)] $1" 2>&1 >>$BPATH$LOG
+F=$(date -d yesterday '+%Y%m%d').log
+cd /home/pi/Dropbox-Uploader
+./dropbox_uploader.sh upload $1 /SouthernCroix/$(hostname)/$1.$F
+}
+#*---------------------------------------------------------------------------------------------
+#* move a file, same as store but deleting the source file afterwards
+#*---------------------------------------------------------------------------------------------
+moveFile() {
+echo `date`" moveFileFile: [$(hostname)] $1" 2>&1 >>$BPATH$LOG
+storeFile $1
+sudo rm -r $1
+
+}
+
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 . /lib/lsb/init-functions
 
@@ -23,13 +52,12 @@ WSPR="/home/pi/whisper/"
 ROLL=`hostname`".maint.log"
 WAV="/home/pi/.local/share/WSJT-X/save/"
 
+
+
 echo "-------------------------------------------------------" 2>&1 >$BPATH$LOG
 echo "Maintenance "$0 "hostname("`hostname`") epoch("`date`")" 2>&1 >>$BPATH$LOG
-cd /home/pi/Dropbox-Uploader
 
-./dropbox_uploader.sh mkdir /SouthernCroix/$(hostname)
-
-./dropbox_uploader.sh upload $BPATH$BUP"(3)" /SouthernCroix/$(hostname)/$BUP"(3)"
+copyFile $BPATH$BUP"(3)"
 
 cd /home/pi
 echo `date`" Removing oldest version" 2>&1 >>$BPATH$LOG
@@ -49,8 +77,6 @@ sudo cp $BPATH$BUP $BPATH$BUP"(1)" 2>&1 >>$BPATH$LOG
 echo `date`" Remove last backup" 2>&1 >>$BPATH$LOG
 sudo -rm -r $BPATH$BUP  2>>$BPATH$ERR >>$BPATH$LOG
 
-echo " " 2>&1 >>$BPATH$LOG
-
 echo `date`" Executing backup path("$APATH") at ("$BPATH$BUP")" 2>&1 >>$BPATH$LOG
 sudo tar -zcf $BPATH$BUP $APATH 2>&1 >>$BPATH$LOG
 
@@ -65,48 +91,17 @@ echo `date`" Pruning Logs("$WSPR")" 2>&1 >>$BPATH$LOG
 F=$(date -d yesterday '+%Y%m%d').log
 echo `date`" Datum for logs is $F" 2>&1 >>$BPATH$LOG
 
+storeFile /home/pi/whisper/wsprRxTx.log
+storeFile /home/pi/whisper/whisper.log
+storeFile /home/pi/.local/share/WSJT-X/ALL_WSPR.TXT
+copyFile /home/pi/$(hostname).host.tar.gz
 
-sudo cat /home/pi/whisper/wsprRxTx.log  > /home/pi/wsprRxTx.$F 2> /dev/null
-#sudo rm -r /home/pi/whisper/wsprRxTx.log 2>>$BPATH$ERR > /dev/null
-
-cd /home/pi/Dropbox-Uploader
-./dropbox_uploader.sh upload /home/pi/wsprRxTx.$F /SouthernCroix/$(hostname)/$(hostname).wsprRxTx.$F
-cd /home/pi
-
-sudo cat  /home/pi/whisper/whisper.log 2>&1 > /home/pi/whisper.$F  
-#sudo rm -r /home/pi/whisper/whisper.log 2>>$BPATH$ERR
-
-cd /home/pi/Dropbox-Uploader
-./dropbox_uploader.sh upload /home/pi/whisper.$F /SouthernCroix/$(hostname)/$(hostname).whisper.$F
-cd /home/pi
-
-sudo cat  /home/pi/.local/share/WSJT-X/ALL_WSPR.TXT 2>&1 > /home/pi/ALL_WSPR.$F 
-#sudo rm -r  /home/pi/.local/share/WSJT-X/ALL_WSPR.TX  2>>$BPATH$ERR
-cd /home/pi/Dropbox-Uploader
-./dropbox_uploader.sh upload /home/pi/ALL_WSPR.$F /SouthernCroix/$(hostname)/$(hostname).ALL_WSPR.$F
-cd /home/pi
+sudo cat /var/log/syslog > /home/pi/syslog
+storeFile  /home/pi/syslog
 
 cd $WAV
 echo `date`" Pruning wsjtx .Wav/.C2 files" 2>>$BPATH$ERR >>$BPATH$LOG
 sudo rm -r *.c2
 for f in `ls -L *.wav | head -n 1000` ; do echo $f; sudo rm -r $f; done
-
-cd /home/pi
-echo `date`" Preserving $(hostname) personality" 2>>$BPATH$ERR >>$BPATH$LOG
-cd /home/pi/Dropbox-Uploader
-./dropbox_uploader.sh upload /home/pi/$(hostname).host.tar.gz /SouthernCroix/$(hostname)/$(hostname).host.tar.gz
-cd /home/pi
-
-sudo cat /var/log/syslog > /home/pi/syslog.$F 2>> /dev/null
-#sudo cat /dev/null > /var/log/syslog
-cd /home/pi/Dropbox-Uploader
-./dropbox_uploader.sh upload /home/pi/syslog.$F /SouthernCroix/$(hostname)/syslog.$F
-cd /home/pi
-
-#sudo rm -r /home/pi/ALL_WSPR.$F
-#sudo rm -r /home/pi/$(hostname).host.tar.gz
-#sudo rm -r /home/pi/syslog.$F
-#sudo rm -r /home/pi/whisper.$F
-#sudo rm -r /home/pi/wsprRxTx.$F
 
 exit 0
