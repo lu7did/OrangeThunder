0823f0f98a516a81e1d1da99510619b3e44cc5a3
lu7did <lu7did@gmail.com>
2019-04-18 20:28:25 -0300

    hostPing

--- bash/hostPing.sh
+++ bash/hostPing.sh
@@ -5,6 +5,13 @@
 #*----------------------------------------------------------------------------
 for i in $(cat /etc/hosts | grep "192." | awk '{ print $2 ; }' | cut -f1 -d"%")
  do
-    echo "$(date) $i "$( ping -c 1 -t 1 $i | grep -a "ttl=" | wc -l )  
+    RESP=$( ping -c 1 -t 1 $i | grep -a "time=")
+    ALIVE=$( ping -c 1 -t 1 $i | grep -a "time=" | wc -l)
+    if [ $ALIVE -eq 1 ]
+    then
+       echo "$(date) $i Response[$(echo $RESP | awk '{ print $8 ; } ' | cut -f2 -d"=") msecs]"
+    else
+        echo "$(date) $i (OFFLINE)"
+    fi 
 done
 
