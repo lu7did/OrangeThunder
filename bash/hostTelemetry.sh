60234eb89e25c44d954a3eff1461322c188879e9
lu7did <lu7did@gmail.com>
2019-04-22 20:15:21 -0300

    hostTelemetry

--- bash/hostTelemetry.sh
+++ bash/hostTelemetry.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-
-cpu='acrux bcrux gacrux kcrux ecrux mucrux magellan'
-for i in $cpu
+#*----------------------------------------------------------------------------
+#* hostTelemetry
+#* Check telemetry on all active nodes
+#*----------------------------------------------------------------------------
+for i in $(cat /etc/hosts | grep "192." | awk '{ print $2 ; }' | cut -f1 -d"%")
  do
-    #p=$(ping -c 1 -t 1 $i | grep -a "icmp" | wc -l)
-    p=$(ssh pi@$i './hostStatus.sh && ./hostCheck.sh' | tail -n 1)
-    echo "TLM($i): $p"
-done
+    RESP=$( ping -c 1 -t 1 $i | grep -a "time=")
+    ALIVE=$( ping -c 1 -t 1 $i | grep -a "time=" | wc -l)
+    if [ $ALIVE -eq 1 ]
+    then
+       p=$(ssh pi@$i './hostStatus.sh && ./hostCheck.sh' | tail -n 1)
+       echo "$p"
 
-#ssh pi@acrux 'sudo shutdown -h now'
-#ssh pi@bcrux 'sudo shutdown -h now'
-#ssh pi@gacrux 'sudo shutdown -h now'
-#ssh pi@kcrux 'sudo shutdown -h now'
-#ssh pi@ecrux 'sudo shutdown -h now'
+    else
+        echo "$(date) $i (OFFLINE)"
+    fi
+done
