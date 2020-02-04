f6f98e697b5f0c6091c84fd78e210c68042a6e62
lu7did <lu7did@gmail.com>
2020-02-04 19:46:42 -0300

    hostPing

--- bash/hostPing.sh
+++ bash/hostPing.sh
@@ -7,11 +7,12 @@ for i in $(cat /etc/hosts | grep "192." | awk '{ print $2 ; }' | cut -f1 -d"%")
  do
     RESP=$( ping -c 1 -t 1 $i | grep -a "time=")
     ALIVE=$( ping -c 1 -t 1 $i | grep -a "time=" | wc -l)
+    IP=$(ping -c 1 -t 1 $i | grep -a ":" | cut -f2 -d"(" | cut -f1 -d")")
     if [ $ALIVE -eq 1 ]
     then
-       echo "$(date) $i Response[$(echo $RESP | awk '{ print $8 ; } ' | cut -f2 -d"=") msecs]"
+       echo "$(date) {$IP} $i Response[$(echo $RESP | awk '{ print $8 ; } ' | cut -f2 -d"=") msecs]"
     else
-        echo "$(date) $i (OFFLINE)"
+        echo "$(date) {$IP} $i (OFFLINE)"
     fi 
 done
 
